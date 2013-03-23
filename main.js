e3ea868cff7b6f422aa35813318e8d79f249e411
schultyy <janschulte90@googlemail.com>
2013-03-23 22:32:19 +0100

    improved http handler. can view build results in client

--- main.js
+++ main.js
@@ -3,6 +3,12 @@ var url = require("url");
 var fs = require("fs");
 var path = require("path");
 
+var options = fs.readFileSync("options.json");
+
+options = JSON.parse(options);
+
+var buildResultsFolder = options.buildResultsFolder;
+
 function logger(){
     
     this.messages = [];
@@ -25,11 +31,32 @@ function logger(){
             date : new Date()
         });
     };
+    this.clear = function(){
+        this.messages = [];
+    };
 }
 
 function getExtension(filename) {
     var ext = path.extname(filename||'').split('.');
     return ext[ext.length - 1];
+}
+
+function getBuildReports(callback){
+    fs.readdir(buildResultsFolder, function(err, files){
+        files.sort().reverse();
+        if(files.length > 10){
+            files = files.slice(0, 10);
+        }
+        callback(files.map(function(item){ return item.replace(".json",""); }));
+    });
+}
+
+function getBuildReport(reportId, callback){
+    var fullPath = path.join(buildResultsFolder, reportId + ".json");
+    fs.readFile(fullPath, function(err, data){
+        if(err) throw err;
+        callback(data);
+    });
 }
 
 var contentTypeMapping = new Array();
@@ -51,12 +78,37 @@ console.log("Reading " + taskFile);
 
 taskFile = require("./" + taskFile.replace(".js", ""));
 
+var log = new logger();
+
+var taskTree = taskFile.getTasks({
+    logger : log,
+    finished : function(buildResult){
+                
+                if(!buildResult){
+                    buildResult = "success";
+                }
+                
+                var filename = new Date().getTime();
+                var absoluteFilename = path.join(buildResultsFolder, filename + ".json");
+                fs.writeFile(absoluteFilename, JSON.stringify({
+                    status : buildResult,
+                    messages : log.messages
+                }, null, 4), function(){ 
+                    taskIsRunning = false; 
+                });
+                console.log("finished build");
+            }
+});
+
 var taskIsRunning = false;
 
 console.log("Starting http server on 127.0.0.1:8080");
 
 http.createServer(function(req, res){    
-var urlPath = url.parse(req.url).path;
+
+    var urlParts = url.parse(req.url);
+
+    var urlPath = urlParts.pathname;
     
     console.log("Handling request " + urlPath);
     
@@ -64,26 +116,33 @@ var urlPath = url.parse(req.url).path;
         return;
     }
     
-    if(urlPath == "/"){
-        urlPath = "index.html";
-    }
-    else if(urlPath == "/startBuild"){
-    
-        builder.buildProject(options, function(log){
-        
-            var logFilename = path.join(options.LogDirectory, "licensemanagement_" + new Date().getTime() + ".json");
-        
-            fs.writeFile(logFilename, JSON.stringify(log.buffer, null, 4), function(err){
-                if(err) 
-                    console.log(err);
-                        
+    switch(urlPath){
+        case "/":
+            urlPath = "index.html";
+            break;
+        case "/projectInfo":
+            res.writeHead(200, {'Content-Type' : 'application/json'});
+            console.log("requested project name " + taskTree.name);		
+            res.write(JSON.stringify({ 
+                projectName : taskTree.name,
+                projectPath : taskTree.projectPath,
+            }));
+            res.end();
+            return;	
+        case "/lastBuilds":        
+            getBuildReports(function(results){
                 res.writeHead(200, {'Content-Type' : 'application/json'});
-                res.write(JSON.stringify(log.buffer));
+                res.write(JSON.stringify(results));
                 res.end();
             });
-
-        });
-        return;
+            return;
+        case "/buildReport":
+            getBuildReport(urlParts.query.replace("id=",""), function(results){
+                res.writeHead(200, {'Content-Type' : 'application/json'});
+                res.write(results);
+                res.end();
+            });
+            return;
     }
     
     var ext = getExtension(urlPath);
@@ -91,29 +150,30 @@ var urlPath = url.parse(req.url).path;
     var filePath = "";
     
     if(urlPath.indexOf("bootstrap") !== -1 ||
-        ext == "js" ||
-        ext == "css"){
-
+                        ext == "js" ||
+                        ext == "css"){
         filePath = "www" + urlPath;
     }
     else{
         filePath = "www/" + ext + "/" + urlPath;
     }
     
-    var file = fs.readFileSync(filePath);
-    
-    if(contentTypeMapping.hasOwnProperty(ext)){
-        var contentType = contentTypeMapping[ext];
+    fs.readFile(filePath, function(err, data){
+        if(err)throw err;
         
-        res.writeHead(200, {'Content-Type' : contentType});
-        res.write(file);
-        res.end();
-    }
-    else{
-        res.writeHead(404, {'Content-Type' : 'text/plain'});
-        res.write("Not found - " + ext);
-        res.end();
-    }
+        if(contentTypeMapping.hasOwnProperty(ext)){
+            var contentType = contentTypeMapping[ext];
+            
+            res.writeHead(200, {'Content-Type' : contentType});
+            res.write(data);
+            res.end();
+        }
+        else{
+            res.writeHead(404, {'Content-Type' : 'text/plain'});
+            res.write("Not found - " + ext);
+            res.end();
+        }        
+    });
 }).listen(8080, '127.0.0.1');
 
 
@@ -122,15 +182,12 @@ setInterval(function(){
     if(taskIsRunning){
         return;
     }
-
     taskIsRunning = true;
-    var log = new logger();
-    var taskTree = taskFile.getTasks(log, function(){
-        console.log("finished build");
-                
-        taskIsRunning = false;
-    });
-    console.log("Running tasks");
-    taskTree.run();    
 
-}, 1500);
+    log.clear();
+    
+    console.log("Running tasks");
+    log.name = taskTree.name;
+    taskTree.tasks.run();    
+
+}, 1 * 60 * 1000);
--- www/js/page.js
+++ www/js/page.js
@@ -1,4 +1,77 @@
 $(document).ready(function(){
-   $("#triggerBuild").click(function(){
-   });
+    $.get("/projectInfo", function(data){
+        $("#projectName").append("<small>" + data.projectName + "</small>");
+        $("#localPath").append("<small>" + data.projectPath + "</small>");
+    });
+    $.get("/lastBuilds", function(resultSet){
+        for(var i = 0; i < resultSet.length; i++){
+        
+            var currentElement = resultSet[i];
+        
+            var time = new Date(parseInt(currentElement));
+
+            var formattedTime = time.getDay() + "." +
+                                time.getMonth() + "." +
+                                time.getFullYear() + " " +
+                                time.getHours() + ':' + time.getMinutes() + ':' + time.getSeconds();
+
+            $("#LatestBuilds").append("<a href='#' id='"+ currentElement +"'>" + formattedTime + "</a>");
+            $("#LatestBuilds").append("<br />"); 
+            
+            $("#" + currentElement).click(function(e){
+            
+                var url = "/buildReport?id=" + currentElement;
+                
+                $.get(url, function(data){
+                    $("#shellOutput").empty();
+
+                    var tabs = $("<ul>").addClass("nav nav-tabs");
+                    
+                    var tabContent = $("<div>").addClass("tab-content");
+                    
+                    for(var i = 0; i < data.messages.length; i++){
+                        var currentElement = data.messages[i];
+                        
+                        var tab = $("<li>");
+                        
+                        if(i == 0){
+                            tab = tab.addClass("active");
+                        }
+                        
+                        if(data.status == "failed"){
+                            tab = tab.addClass("buildFailed");
+                        }
+                        
+                        var heading = $("<a>")
+                                        .attr("data-toggle","tab")
+                                        .attr("href", "#" + currentElement.name)
+                                        .html(currentElement.name);
+                        
+                        tab.append(heading);
+                        tabs.append(tab);
+                        
+                        var content = $("<div>").addClass("tab-pane");
+                        
+                        if(i == 0){
+                            content = content.addClass("active");
+                        }
+                        
+                        content = content.attr("id", currentElement.name)
+                                            .html($("<code>")
+                                            .addClass("shellContent")
+                                            .html("<pre>" + currentElement.message + "</pre>"));
+                        
+                        tabContent.append(content);
+                    }
+                    $('a[data-toggle="tab"]').on('show', function (e) {
+                        alert("buh");
+                    });
+                    $("#shellOutput").append(tabs);
+                    $("#shellOutput").append(tabContent);
+                });
+            
+                e.preventDefault();
+            });
+        }
+    });
 });
